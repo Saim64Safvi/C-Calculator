f6a14fc837ffdc1044f9b4f3ce37f2c3f51ef01b
Saim64Safvi <103188328+Saim64Safvi@users.noreply.github.com>
2023-07-26 13:11:33 +0500

    Add files via upload

---
+++ Calculator.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+char op;
+
+int firstNum;
+int secondNum;
+int thirdNum;
+int fourthNum;
+
+
+printf("Choose an operator:\n\n");
+printf("Enter + for Addition:\n");
+printf("Enter - for Subtraction:\n");
+printf("Enter * for Multiplication:\n");
+printf("Enter / for Division:\n");
+printf("Enter S to find Square a number:\n");
+printf("Enter C to find Cube of a number:\n");
+printf("Enter s to find Square Root of a number:\n");
+printf("Enter c to find Cube Root of a number:\n");
+printf("Enter R to Round a number:\n");
+printf("Enter P to find Power of a number:\n");
+
+switch(op){
+
+case +:
+
+printf("\n\nYou have chosen Addition: \n");
+printf("\nEnter the first Number: \n");
+scanf("%d", &firstNum);
+printf("\nEnter the second Number: \n");
+scanf("%d", &secondNum);
+
+
+
+}
+
+}
